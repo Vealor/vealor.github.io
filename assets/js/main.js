2dd4e7c0e5d84fef8a8ac76e7a218506fa89b575
Vealor <jakob.m.roberts@gmail.com>
2017-11-01 13:36:39 +0900

    Update with content - need a new head picture

--- assets/js/main.js
+++
@@ -1,3 +0,0 @@
-function getSection(section) {
-     document.getElementById("content-section").innerHTML= '<object type="text/html" data="' + section + '.html" style="height: 100%; width: 100%;"></object>';
-}
